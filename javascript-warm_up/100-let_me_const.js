c0a315e5a68d6f97a29049c311aadf07dd164191
mide1020 <akinrefonayomide1@gmail.com>
2023-09-04 17:59:00 +0100

    task 5

---
+++ javascript-warm_up/100-let_me_const.js
@@ -0,0 +1,8 @@
+// Assuming myVar is declared somewhere in your code
+let myVar = 123; // Replace 123 with the initial value of myVar
+
+// Modify the value of myVar
+myVar = 333;
+
+// You can print the modified value to verify it
+console.log(myVar);
