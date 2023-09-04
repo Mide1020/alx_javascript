ba32562901662390c04b076e44959e2a7cfddfa8
mide1020 <akinrefonayomide1@gmail.com>
2023-09-04 13:15:17 +0100

    task 3

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,8 +1,6 @@
 const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
 for (let i = 0; i < lines.length; i++) {
-  if (lines[i].includes("12")) {
-    lines[i] = lines[i].replace("12", "89");
-  }
+  lines[i] = lines[i].replace("12", "89");
   console.log(lines[i]);
 }
