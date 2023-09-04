c7e6765b9eed550ebc417c26118da7461a58aee5
mide1020 <akinrefonayomide1@gmail.com>
2023-09-04 13:23:41 +0100

    task 4

---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,12 @@
+// Define the add function
+function add(x, y) {
+    return x + y;
+  }
+  
+  // Export the function if needed (e.g., for use in another module)
+  // module.exports = add; // For Node.js/CommonJS
+  // export default add; // For ES6 Modules
+  
+  // Example usage:
+  const result = add(5, 7);
+  console.log(result); // This will print 12 to the console  
