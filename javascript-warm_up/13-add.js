ef8e7c27f646995d014432c51d92cdafa16fbe3b
mide1020 <akinrefonayomide1@gmail.com>
2023-09-04 17:03:35 +0100

    task 4

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -4,4 +4,4 @@ function add(x, y) {
   
   // Example usage:
   console.log(add(93, -4)); // This should print 89 to the console
-  console.log(add(0, 89));  // This should print 89 to the console  
+  console.log(add(0, 89));  // This should print 89 to the console
