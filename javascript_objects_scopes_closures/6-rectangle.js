3282dbf4f24517c6c60741af5504754163cb9271
mide1020 <akinrefonayomide1@gmail.com>
2023-09-07 17:36:14 +0100

    task 6

---
+++ javascript_objects_scopes_closures/6-rectangle.js
@@ -0,0 +1,25 @@
+/ 6-square.js
+
+const SquareParent = require('./5-square');
+
+class Square extends SquareParent {
+
+  charPrint(c) {
+
+    if (c === undefined) {
+
+      c = 'X';
+
+    }
+
+    for (let i = 0; i < this.height; i++) {
+
+      console.log(c.repeat(this.width));
+
+    }
+
+  }
+
+}
+
+module.exports = Square;
