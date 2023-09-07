448b950afc2289319dff0fa0cd9cc61da80954b2
mide1020 <akinrefonayomide1@gmail.com>
2023-09-07 15:11:53 +0100

    task 6

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,10 +1,10 @@
 //.call_me_moby.js
 
 //Define the callMeMoby function
-functioncallMeMobby(x, theFunction)
+function callMeMoby(x, theFunction)
 {
     for (let i = 0; i < x; i++) {
-        theFunction
+        theFunction();
     }
 }
 // Export the callMeMobby function to make it visible from outside
