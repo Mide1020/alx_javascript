a98cba1ac457319ade250c143341d363375aba18
mide1020 <akinrefonayomide1@gmail.com>
2023-09-07 17:32:36 +0100

    Task 4

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,49 @@
+4-rectangle.js
+
+class Rectangle {
+
+    constructor(w, h) {
+
+      if (w > 0 && h > 0) {
+
+        this.width = w;
+
+        this.height = h;
+
+      } else {
+
+        // Create an empty object if w or h is not a positive integer or is 0
+
+        return {};
+
+      }
+
+    }
+
+    print() {
+
+      for (let i = 0; i < this.height; i++) {
+
+        console.log('X'.repeat(this.width));
+
+      }
+
+    }
+
+    rotate() {
+
+      [this.width, this.height] = [this.height, this.width];
+
+    }
+
+    double() {
+
+      this.width *= 2;
+
+      this.height *= 2;
+
+    }
+
+  }
+
+  module.exports = Rectangle;
