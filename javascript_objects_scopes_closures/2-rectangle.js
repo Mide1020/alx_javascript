49e6dce3375a2a33eb3af72193a540ff40f8f873
mide1020 <akinrefonayomide1@gmail.com>
2023-10-11 03:39:53 +0100

    2- rectangle

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,56 +1,15 @@
 #!/usr/bin/node
 class Rectangle {
 
-    constructor(w, h) {
-
-      if (w > 0 && h > 0) {
-
-        this.width = w;
-
-        this.height = h;
-
-      } else {
-
-        // Create an empty object if w or h is not a positive integer or is 0
-
-        return {};
-
+  // initialize an instance with these values
+  constructor (w, h) {
+      if (w && h > 0) {
+          this.width = w;
+          this.height = h;
+      }else {
+          return 'Rectangle {}';
       }
-
-    }
-
   }
+}
 
-  module.exports = Rectangle;
-
-  if (require.main === module) {
-
-    const args = process.argv.slice(2);
-
-    if (args.length === 2) {
-
-      const width = parseInt(args[0]);
-
-      const height = parseInt(args[1]);
-
-      const r = new Rectangle(width, height);
-
-      console.log(`Instance width: ${r.width} - height: ${r.height}`);
-
-    } else if (args.length === 1) {
-
-      const width = parseInt(args[0]);
-
-      const r = new Rectangle(width);
-
-      console.log(`Instance width: ${r.width}`);
-
-    } else {
-
-      const r = new Rectangle();
-
-      console.log(`Instance no argument`);
-
-    }
-
-  }
+module.exports = Rectangle;
